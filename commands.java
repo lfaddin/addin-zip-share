8a91bb2419e4e2946e6d69384ebcc05cd1a6240a
lfaddin <cbonazza@lineafabbrica.it>
2025-06-25 16:37:46 +0200

    Add files via upload

---
+++ commands.java
@@ -0,0 +1,6 @@
+// Questo file è richiesto dal manifest.xml
+// Non contiene logica specifica per questo add-in,
+// ma è necessario per evitare errori di caricamento.
+
+// Puoi aggiungere qui funzioni Office.js se in futuro
+// deciderai di usare comandi della ribbon o altre funzionalità.
